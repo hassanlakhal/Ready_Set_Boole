e0cdaa726ab5bdace1a26d967f259f7667cd763a
Hassan Lakhal <hlakhal.contact@gmail.com>
2026-07-08 11:45:14 +0100

    add function of adder

--- ex00/src/main.rs
+++ ex00/src/main.rs
@@ -1,3 +1,12 @@
+fn adder(a: u32, b: u32) -> u32{
+    let sum_bit = a ^ b;
+    let carry_bit = a & b;
+
+    let resule = (carry_bit << 1) | sum_bit;
+    resule  
+}
+
 fn main() {
-    println!("Hello, world!");
+    let sum = adder(1,1336);
+    println!("{}",sum);
 }
